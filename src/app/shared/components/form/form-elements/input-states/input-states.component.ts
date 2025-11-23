56a1a6c6064417b0eef2d74d6cc43c5a862508a0
dromeroma <deimerromeromadera@gmail.com>
2025-11-23 16:24:45 -0500

    Ajuste componente input

--- src/app/shared/components/form/form-elements/input-states/input-states.component.ts
+++ src/app/shared/components/form/form-elements/input-states/input-states.component.ts
@@ -3,37 +3,33 @@ import { Component } from '@angular/core';
 import { ComponentCardComponent } from '../../../common/component-card/component-card.component';
 import { InputFieldComponent } from '../../input/input-field.component';
 import { LabelComponent } from '../../label/label.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-input-states',
   imports: [
     CommonModule,
+    FormsModule,
     ComponentCardComponent,
     InputFieldComponent,
     LabelComponent
   ],
   templateUrl: './input-states.component.html',
-  styles: ``
 })
 export class InputStatesComponent {
 
   email = '';
   emailTwo = 'hello.pimjo@gmail.com';
   error = false;
+  emailThree = 'disabled@example.com';
 
-  validateEmail(value: string): boolean {
-    const isValidEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
-    this.error = !isValidEmail;
-    return isValidEmail;
+  validateEmail() {
+    const value = this.email;
+    this.error = !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
   }
 
-  handleEmailChange(value: string | number) {
-    this.email = value.toString();
-    this.validateEmail(this.email);
-  }
-  
-  handleEmailTwoChange(value: string | number) {
-    this.emailTwo = value.toString();
-    this.validateEmail(this.emailTwo);
+  validateEmailTwo() {
+    const value = this.emailTwo;
+    this.error = !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(value);
   }
 }
